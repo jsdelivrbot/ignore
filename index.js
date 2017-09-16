e7305d0c7a3e7ca956e070fc5bf8f1183e7912e2
admin <admin@admins-MBP.fios-router.home>
2017-09-16 01:13:11 -0400

    Test De,p

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var cool = require('cool-ascii-faces');
 var app = express();
 
 app.set('port', (process.env.PORT || 5000));
@@ -16,7 +15,8 @@ app.get('/', function(request, response) {
 
 app.get('/cool', function(request, response){
     response.send();
-})
+});
+
 
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
